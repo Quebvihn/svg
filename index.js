84831aa6d45d1a82f1ae3891019c899af91ef73f
Quebvihn <bamedinatorres@gmail.com>
2023-10-23 16:54:01 -0400

    text qs

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 
 
-function promptUser() {
+function questions() {
     inquirer
       .prompt([
         {
@@ -10,6 +10,18 @@ function promptUser() {
             message: "What shape would you like the logo to render?",
             choices: ["Triangle", "Square", "Circle"],
             name: "shape",
+          },
+          {
+            type: "input",
+            message:
+              "What text would you like you logo to display? (Enter up to three characters)",
+            name: "text",
+          },
+          {
+            type: "input",
+            message:
+              "Choose text color (Enter color keyword OR a hexadecimal number)",
+            name: "textColor",
           },
       ])
       .then((answers) => {
