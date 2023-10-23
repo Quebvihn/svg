bb410b6fe68274380ccd7b2fe4501379aa688694
Quebvihn <bamedinatorres@gmail.com>
2023-10-23 16:50:59 -0400

    shapes prompt

--- index.js
+++ index.js
@@ -5,7 +5,12 @@ const fs = require("fs");
 function promptUser() {
     inquirer
       .prompt([
-
+        {
+            type: "list",
+            message: "What shape would you like the logo to render?",
+            choices: ["Triangle", "Square", "Circle"],
+            name: "shape",
+          },
       ])
       .then((answers) => {
     })
