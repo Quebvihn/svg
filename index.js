59edeeea0b4a98e488bd535cd5421ebc8a782498
Quebvihn <bamedinatorres@gmail.com>
2023-10-23 17:35:03 -0400

    shape

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ const fs = require("fs");
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
 function writeToFile(filename, answers){
-    
+    let svgString = "";
+  
+  svgString =
+    '<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">';
+  
+  svgString += "<g>";
+  
+  svgString += `${answers.shape}`;
 }
 
 
